5127de9c689bd413cb646bf8ffa752b98ca3764e
hainguyen <hainguyen8086@gmail.com>
2026-06-02 15:02:35 +0700

    add redis adapter

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from 'next';
 const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   async rewrites() {
     return {
       beforeFiles: [
